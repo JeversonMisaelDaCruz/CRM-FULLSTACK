dc1dfb8797f81d097c22fe6edf108767150a6bcd
Jeverson misael da cruz filho <jeversonmisaldacruzfilho@gmail.com>
2024-11-11 16:32:36 -0300

    FEAT: Pipeline phase E2E tests

--- backend/src/pipeline_phase/tests/pipelinePhase.const.e2e.ts
+++ backend/src/pipeline_phase/tests/pipelinePhase.const.e2e.ts
@@ -1,5 +1,10 @@
+
 export const newPipelinePhase = {
   name: 'Ganho',
+};
+
+export const updatePipelinePhaseData = {
+  name: 'Perdido',
 };
 
 export const pipelinePhaseWrongData = {
--- backend/src/pipeline_phase/tests/pipelinePhase.e2e-spec.ts
+++ backend/src/pipeline_phase/tests/pipelinePhase.e2e-spec.ts
@@ -1,16 +1,21 @@
+// pipelinePhase.e2e-spec.ts
+
 import * as request from 'supertest';
 import { app, authToken } from '../../global.e2e-spec';
 import {
   newPipelinePhase,
   pipelinePhaseWrongData,
+  updatePipelinePhaseData,
 } from './pipelinePhase.const.e2e';
 
+// Testes de criação
 it('Criando uma Pipeline Phase faltando autenticação', async () => {
   return await request(app.getHttpServer())
     .post('/pipeline-phase')
     .send(newPipelinePhase)
     .expect(401);
 });
+
 it('Criando uma Pipeline Phase na rota incorreta', async () => {
   return await request(app.getHttpServer())
     .post('/pipeline-phases')
@@ -18,6 +23,7 @@ it('Criando uma Pipeline Phase na rota incorreta', async () => {
     .send(newPipelinePhase)
     .expect(404);
 });
+
 it('Criando uma Pipeline Phase com os dados incorretos', async () => {
   return await request(app.getHttpServer())
     .post('/pipeline-phase')
@@ -25,6 +31,7 @@ it('Criando uma Pipeline Phase com os dados incorretos', async () => {
     .send(pipelinePhaseWrongData)
     .expect(400);
 });
+
 it('Criando uma Pipeline Phase', async () => {
   return await request(app.getHttpServer())
     .post('/pipeline-phase')
@@ -33,37 +40,123 @@ it('Criando uma Pipeline Phase', async () => {
     .expect(201);
 });
 
-it('listando todas as pipelines', async () => {
+// Testes de listagem
+it('Listando todas as Pipeline Phases', async () => {
   return await request(app.getHttpServer())
     .get('/pipeline-phase')
     .set('Authorization', `Bearer ${authToken}`)
     .expect(200);
 });
-it('listando todas as pipelines na rota incorreta', async () => {
+
+it('Listando todas as Pipeline Phases na rota incorreta', async () => {
   return await request(app.getHttpServer())
     .get('/pipeline-phases')
     .set('Authorization', `Bearer ${authToken}`)
     .expect(404);
 });
-it('listando todas as pipelines sem autenticação', async () => {
+
+it('Listando todas as Pipeline Phases sem autenticação', async () => {
   return await request(app.getHttpServer()).get('/pipeline-phase').expect(401);
 });
 
-it('listando uma pipeline pelo id', async () => {
+it('Listando uma Pipeline Phase pelo ID', async () => {
   const response = await request(app.getHttpServer())
     .get('/pipeline-phase')
     .set('Authorization', `Bearer ${authToken}`)
     .expect(200);
-  const pipelinePhase = Array.isArray(response.body)
+  const pipelinePhases = Array.isArray(response.body)
     ? response.body
     : response.body.data;
-  expect(pipelinePhase).toBeInstanceOf(Array);
-  expect(pipelinePhase.length).toBeGreaterThan(0);
+  expect(pipelinePhases).toBeInstanceOf(Array);
+  expect(pipelinePhases.length).toBeGreaterThan(0);
 
-  const pipelinePhaseId = pipelinePhase[0].id;
+  const pipelinePhaseId = pipelinePhases[0].id;
 
   return await request(app.getHttpServer())
     .get(`/pipeline-phase/${pipelinePhaseId}`)
+    .set('Authorization', `Bearer ${authToken}`)
+    .expect(200);
+});
+
+// Testes de atualização
+it('Atualizando uma Pipeline Phase sem autenticação', async () => {
+  const response = await request(app.getHttpServer())
+    .get('/pipeline-phase')
+    .set('Authorization', `Bearer ${authToken}`)
+    .expect(200);
+  const pipelinePhases = Array.isArray(response.body)
+    ? response.body
+    : response.body.data;
+  const pipelinePhaseId = pipelinePhases[0].id;
+
+  return await request(app.getHttpServer())
+    .patch(`/pipeline-phase/${pipelinePhaseId}`)
+    .send(updatePipelinePhaseData)
+    .expect(401);
+});
+
+it('Atualizando uma Pipeline Phase com dados incorretos', async () => {
+  const response = await request(app.getHttpServer())
+    .get('/pipeline-phase')
+    .set('Authorization', `Bearer ${authToken}`)
+    .expect(200);
+  const pipelinePhases = Array.isArray(response.body)
+    ? response.body
+    : response.body.data;
+  const pipelinePhaseId = pipelinePhases[0].id;
+
+  return await request(app.getHttpServer())
+    .patch(`/pipeline-phase/${pipelinePhaseId}`)
+    .set('Authorization', `Bearer ${authToken}`)
+    .send(pipelinePhaseWrongData)
+    .expect(400);
+});
+
+it('Atualizando uma Pipeline Phase com sucesso', async () => {
+  const response = await request(app.getHttpServer())
+    .get('/pipeline-phase')
+    .set('Authorization', `Bearer ${authToken}`)
+    .expect(200);
+  const pipelinePhases = Array.isArray(response.body)
+    ? response.body
+    : response.body.data;
+  const pipelinePhaseId = pipelinePhases[0].id;
+
+  return await request(app.getHttpServer())
+    .patch(`/pipeline-phase/${pipelinePhaseId}`)
+    .set('Authorization', `Bearer ${authToken}`)
+    .send(updatePipelinePhaseData)
+    .expect(200);
+});
+
+// Testes de exclusão
+it('Deletando uma Pipeline Phase sem autenticação', async () => {
+  const response = await request(app.getHttpServer())
+    .get('/pipeline-phase')
+    .set('Authorization', `Bearer ${authToken}`)
+    .expect(200);
+  const pipelinePhases = Array.isArray(response.body)
+    ? response.body
+    : response.body.data;
+  const pipelinePhaseId = pipelinePhases[0].id;
+
+  return await request(app.getHttpServer())
+    .delete(`/pipeline-phase/${pipelinePhaseId}`)
+    .expect(401);
+});
+
+it('Deletando uma Pipeline Phase com sucesso', async () => {
+  const response = await request(app.getHttpServer())
+    .get('/pipeline-phase')
+    .set('Authorization', `Bearer ${authToken}`)
+    .expect(200);
+  const pipelinePhases = Array.isArray(response.body)
+    ? response.body
+    : response.body.data;
+  const pipelinePhaseId = pipelinePhases[0].id;
+
+  return await request(app.getHttpServer())
+    .delete(`/pipeline-phase/${pipelinePhaseId}`)
     .set('Authorization', `Bearer ${authToken}`)
     .expect(200);
 });
