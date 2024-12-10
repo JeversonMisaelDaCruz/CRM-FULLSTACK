380f5180f70b64b9ef1c2b0a57ca3d94f5c12f36
Jeverson misael da cruz filho <jeversonmisaldacruzfilho@gmail.com>
2024-12-10 08:14:42 -0300

    Binary  files

--- frontend/src/views/Leads.vue
+++ frontend/src/views/Leads.vue
@@ -11,7 +11,7 @@
       />
       <v-data-table
         :headers="headers"
-        :items="leads"
+        :items="leads"d
         class="elevation-1 mt-4"
         style="background-color: #dfd8c3; color: black"
       >
